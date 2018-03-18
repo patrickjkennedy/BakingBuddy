5441ceb07782b654a87d93e6304ec2ef5de225d4
pkennedy89 <patrickken@gmail.com>
2018-03-18 16:58:15 +0000

    added & started work on MasterListFragment class

---
+++ app/src/main/java/com/example/android/bakingbuddy/ui/MasterListFragment.java
@@ -0,0 +1,69 @@
+package com.example.android.bakingbuddy.ui;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.android.bakingbuddy.R;
+
+/**
+ * Created by pkennedy on 3/18/18.
+ */
+
+public class MasterListFragment extends Fragment {
+
+    // Context
+    private Context mContext;
+
+    // RecyclerView
+    private RecyclerView mRecyclerView;
+    private Adapter mAdapter;
+    private LinearLayoutManager mLayoutManager;
+
+    // TextView for Error Messaging
+    private TextView mErrorTextView;
+
+    // ProgressBar for Loading
+    private ProgressBar mLoadingIndicator;
+
+    // Mandatory empty constructor
+    public MasterListFragment(){
+    }
+
+    // Inflates the RecyclerView containing the CardViews for each recipe
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Context
+        mContext = getActivity();
+
+        final View rootView = inflater.inflate(R.layout.fragment_master_list, container, false);
+
+        // Recyclerview
+        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.rv_master_list);
+
+        // TextView for Error Messaging
+        mErrorTextView = (TextView) rootView.findViewById(R.id.tv_error_message_display);
+
+        // ProgressBar for Loading Indicator
+        mLoadingIndicator = (ProgressBar) rootView.findViewById(R.id.pb_loading_indicator);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // Creating a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //TODO: Create and  Initialize the recyclerview adapter
+
+        return rootView;
+    }
+}
