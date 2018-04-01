1f8ad684ed01e51e8292627ee15403aa1868f4d1
pkennedy89 <patrickken@gmail.com>
2018-04-01 20:08:16 +0100

    need to debug blank activity

--- app/src/main/java/com/example/android/bakingbuddy/ui/OverviewActivity.java
+++ app/src/main/java/com/example/android/bakingbuddy/ui/OverviewActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.bakingbuddy.ui;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,24 +10,58 @@ import com.example.android.bakingbuddy.data.OverviewFragmentPagerAdapter;
 
 public class OverviewActivity extends AppCompatActivity {
 
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_overview);
 
-        // Find the view pager that will allow the user to swipe between fragments
-        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        if(findViewById(R.id.overview_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
 
-        // Create an adapter that knows which fragment should be shown on each page
-        OverviewFragmentPagerAdapter adapter = new OverviewFragmentPagerAdapter(getSupportFragmentManager());
+            if(savedInstanceState == null) {
 
-        // Set the adapter onto the view pager
-        viewPager.setAdapter(adapter);
+                // Add the Steps Fragment to the screen
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                StepsFragment stepsFragment = new StepsFragment();
 
-        // Give the TabLayout the ViewPager
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
-        tabLayout.setupWithViewPager(viewPager);
+                // Add the fragment to its container using a transaction
+                fragmentManager.beginTransaction().add(R.id.overview_steps_container, stepsFragment);
+
+                // Add the Detail Fragment to the screen
+                DetailFragment detailFragment = new DetailFragment();
+
+                // Add the the fragment
+                fragmentManager.beginTransaction().add(R.id.overview_detail_container, detailFragment);
+            }
+
+        } else{
+            mTwoPane = false;
+
+            // Find the view pager that will allow the user to swipe between fragments
+            ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+            // Create an adapter that knows which fragment should be shown on each page
+            OverviewFragmentPagerAdapter adapter = new OverviewFragmentPagerAdapter(getSupportFragmentManager());
+
+            // Set the adapter onto the view pager
+            viewPager.setAdapter(adapter);
+
+            // Give the TabLayout the ViewPager
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+            tabLayout.setupWithViewPager(viewPager);
+
+        }
+
     }
 
     public void setActionBarTitle(String title){
